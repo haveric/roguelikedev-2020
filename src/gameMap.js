294f8219f97569f4f072bf5a241db8d8a8833f2d
Ryan Breuer <breuerryan@gmail.com>
2020-07-27 12:55:38 -0500

    Fix description sorting on initial item drop

--- src/gameMap.js
+++ src/gameMap.js
@@ -66,7 +66,7 @@ export default class GameMap {
             }
         }
 
-        this.entities.sort(_entityDepthCompare);
+        entities.sort(_entityDepthCompare);
 
         return entities;
     }
