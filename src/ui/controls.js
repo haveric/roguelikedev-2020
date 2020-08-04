764aa8ee74395e874adc0658f811533843344e38
holmat <andrew.t.holm@gmail.com>
2020-08-03 22:16:13 -0500

    Adjust controls

--- src/ui/controls.js
+++ src/ui/controls.js
@@ -2,8 +2,8 @@ export const CONTROLS = `
     Here are some controls to get you started...
      Use the numpad or arrow keys to move.
      Use Numpad5 to WAIT.
-     G to Pickup, I to access Inventory.
-     Use D to drop items.
+     G to Pickup, Use D to drop items.
+     I to access Inventory. E to equip.
      Use O and C to open and close.
      Use HOME to debug, PAGEDOWN to enter debug room.
      ENTER to take stairs.
